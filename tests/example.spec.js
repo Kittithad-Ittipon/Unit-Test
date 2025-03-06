e72a01285487a1114543b1086f1d5bb3c03143b4
Kittithad-Ittipon <a.kittithad.ittitpon@gmail.com>
2025-03-06 16:57:52 +0700

    update api

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,51 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+var user_id; 
+// การทดสอบ api get
+test('Get user', async ({ request }) => {
+    const response = await request.get('https://reqres.in/api/users?page=2');
+    const body = await response.json();
+    console.log(response);
+    console.log(body);
+    expect(response.status()).toBe(200);
+});
+// การทดสอบ api post
+test('login', async ({ request }) => {
+    const response = await request.post('https://reqres.in/api/login' , {
+        data :{ "email": "eve.holt@reqres.in" , "password": "cityslicka"},
+        headers: {"Accept" : "application/json"}
+    });
+    // console.log(response1);
+    expect(response.status()).toBe(200);
+    var res = await response.json();
+    var token = res.token
+    // console.log(res);
+    console.log('token is :'+token);
+
+});
+// การทดสอบ api post
+test('show id', async ({ request }) => {
+    const response = await request.post('https://reqres.in/api/users' , {
+        data :{ "name": "morpheus", "job": "leader"},
+        headers: {"Accept" : "application/json"}
+    });
+    expect(response.status()).toBe(201);
+    var res = await response.json();
+    user_id = res.id
+    console.log('id is :'+user_id);
+});
+test('put', async ({ request }) => {
+    const response = await request.put('https://reqres.in/api/users/2' , {
+        data :{ "name": "Kit", "job": "people"},
+        headers: {"Accept" : "application/json"}
+    });
+    var res = await response.json();
+    console.log("res is : "+res.name +" "+ res.job);
+    expect(response.status()).toBe(200);
+});
+test('del', async ({ request }) => {
+    const response = await request.delete('https://reqres.in/api/users/2');
+    expect(response.status()).toBe(204);
+});
